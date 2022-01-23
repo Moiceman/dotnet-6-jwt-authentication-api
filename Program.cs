8e9c77134ecbc9f0d7cc3a90e360b1a54c3b512b
Icem@n <moi.ceman+git@gmail.com>
2022-01-23 10:45:11 +0100

    Add SwaggerUI

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using WebApi.Helpers;
+﻿using Microsoft.OpenApi.Models;
+using WebApi.Helpers;
 using WebApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,30 @@ var builder = WebApplication.CreateBuilder(args);
     var services = builder.Services;
     services.AddCors();
     services.AddControllers();
+
+    services.AddSwaggerGen(option => {
+        option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+        });
+        
+        option.AddSecurityRequirement(new OpenApiSecurityRequirement()
+        {
+        {
+            new OpenApiSecurityScheme{
+                Reference = new OpenApiReference{
+                    Id = "Bearer", //The name of the previously defined security scheme.
+                    Type = ReferenceType.SecurityScheme
+                }
+            },
+            new string[] {}
+        }});
+    });
 
     // configure strongly typed settings object
     services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
@@ -20,6 +45,9 @@ var app = builder.Build();
 
 // configure HTTP request pipeline
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
     // global cors policy
     app.UseCors(x => x
         .AllowAnyOrigin()
